ed2205974819e710b6c96c6ad86d05af7d04bfb3
Oscar Lucas <olucas@espol.edu.ec>
2023-06-08 22:52:44 -0500

    CHALLENGE WORKSHOP II DONE

--- vacation-package-cost-estimator/src/main/java/Main.java
+++ vacation-package-cost-estimator/src/main/java/Main.java
@@ -1,73 +1,94 @@
+
 // Copyright (C) 2020
 // All rights reserved
 import java.util.Scanner;
 
-
 public class Main {
 
-    /**
+	/**
 	 * Summary of JavaDoc will be here.
 	 *
 	 * @author Oscar Lucas
-	 * @param args Arguments 
+	 * @param args Arguments
 	 * 
 	 */
-    public static void main(String[] args) {
-		
-	System.out.println("Vacation Package Cost Estimator System");
-	
-	Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
 
-	// Get user input
-	System.out.print("Enter destination of the vacation: ");
-	String destination = scanner.nextLine();
-	System.out.println("");
+		Scanner scanner = new Scanner(System.in);
 
-	System.out.print("Enter number of travelers: ");
-	int numTravelers = scanner.nextInt();
-	System.out.println("");
+		System.out.println("Enter the destination of the vacation (Paris, New York City):");
+		String destination = scanner.nextLine();
 
-	System.out.print("Enter duration of the vacation in days: ");
-	int duration = scanner.nextInt();
-	System.out.println("");
+		System.out.println("Enter the number of travelers:");
+		int numTravelers = scanner.nextInt();
 
-	// Calculate total cost
-	int baseCost = 1000;
-	int additionalCost = 0;
-	int groupDiscount = 0;
-	int reduceFee = 0;
-	int promotionDiscount = 0;
+		System.out.println("Enter the duration of the vacation in days:");
+		int duration = scanner.nextInt();
 
-	if (destination.equalsIgnoreCase("Paris")) {
-            additionalCost = 500;
-	} else if (destination.equalsIgnoreCase("New York City")) {
-            additionalCost = 600;
+		int baseCost = 1000;
+		int additionalCost = 0;
+
+		// Calculate additional cost based on destination
+		if ("Paris".equalsIgnoreCase(destination)) { 
+			additionalCost = 500;
+		} else if ("New York City".equalsIgnoreCase(destination)) {  
+			additionalCost = 600;
+		}
+
+		int totalCost = baseCost + additionalCost;
+
+		// Apply group discount
+		if (numTravelers > 4 && numTravelers < 10) {
+			totalCost -= totalCost * 0.1; // 10% discount
+		} else if (numTravelers > 10) {
+			totalCost -= totalCost * 0.2; // 20% discount
+		}
+
+		// Apply penalty fee for duration less than 7 days
+		if (duration < 7) {
+			totalCost += 200;
+		}
+
+		// Apply promotion policy
+		if (duration > 30 || numTravelers == 2) {
+			totalCost -= 200;
+		}
+
+		// Check for group size limit
+		if (numTravelers > 80) {
+			System.out.println("The vacation package is not available for groups of more than 80 persons.");
+			totalCost = -1;
+		}
+
+		// Add-ons selection
+		System.out.println("Select optional add-ons (Y/N):");
+		String addOns = scanner.next();
+
+		int addOnsCost = 0;
+
+		if ("Y".equalsIgnoreCase(addOns)) {  
+			System.out.println("All-Inclusive Package - $200 per traveler");
+			System.out.println("Adventure Activities Package - $150 per traveler");
+			System.out.println("Spa and Wellness Package - $100 per traveler");
+
+			System.out.println("Enter the number of travelers for each add-on:");
+			int numAllInclusive = scanner.nextInt();
+			int numAdventure = scanner.nextInt();
+			int numSpa = scanner.nextInt();
+
+			addOnsCost = (numAllInclusive * 200) + (numAdventure * 150) + (numSpa * 100);
+		}
+
+		// Calculate total cost with add-ons
+		totalCost += addOnsCost;
+
+		if (totalCost >= 0) {
+			System.out.println("Total cost of the vacation package: $" + totalCost);
+		} else {
+			System.out.println("Invalid input data.");
+		}
+
+		scanner.close();
 	}
-	if (numTravelers > 4 && numTravelers < 10) {
-            groupDiscount = 10;
-	} else if (numTravelers > 10) {
-            groupDiscount = 20;
-	}
-	if (duration < 7) {
-            reduceFee = 200;
-	}
-	if (duration > 30 || numTravelers == 2) {
-            promotionDiscount = 200;
-	}
-	int totalCost = baseCost + additionalCost - promotionDiscount - reduceFee;
-	totalCost -= (totalCost * groupDiscount) / 100;
-	// Check if the input data is valid
-	if (totalCost < 0 || numTravelers > 80) {
-            totalCost = -1;
-	}
-	// Print the total cost or error message
-	if (totalCost >= 0) {
-            System.out.println("Total cost of the vacation package: $" + totalCost);
-	} else {
-            System.out.println("Invalid input data. Please check your inputs.");
-	}
-	
-	scanner.close();
-    }
 
 }
